bf2e69ad0e92a0235fd9926360af95046643eb17
Andrés Mauricio Acosta Pulido <andres.taamap@gmail.com>
2019-03-28 15:32:21 -0500

    change state login, and change template

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,7 +1,21 @@
 import { Component } from '@angular/core';
+import * as myGlobals from 'src/globals'; //<==== this one
+import { variable } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: 'login.component.html'
 })
-export class LoginComponent { }
+export class LoginComponent {
+  public iniciar = true;
+  public variable = myGlobals.logeado;
+  constructor() {
+    this.variable = this.iniciar;
+  }
+  /**
+   * changeStateLogin
+   */
+  public changeStateLogin() {
+    this.variable = this.iniciar;
+  }
+}
---
+++ src/globals.ts
@@ -0,0 +1,6 @@
+//
+// ===== File globals.ts    
+//
+'use strict';
+
+export var logeado=false;

010fed5b81246da338d4cfedff0c6937a7d1527e
Felipe Cano <felipecanol@gmail.com>
2019-04-11 11:53:51 -0500

    Se ajustó la presentación de la consulta básica

--- src/app/pages/consults/basic-parcel-info/basic-parcel-info.component.ts
+++ src/app/pages/consults/basic-parcel-info/basic-parcel-info.component.ts
@@ -2,16 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { BasicConsultService } from 'src/app/services/consult/basic-consult.service';
 import { BasicConsult } from 'src/app/models/basic-parcel-info.interface';
 
+import PluggableMap from 'ol/PluggableMap.js';
 import Map from 'ol/Map';
 import View from 'ol/View';
 import LayerTile from 'ol/layer/Tile';
 import XYZ from 'ol/source/XYZ';
-import { fromLonLat } from 'ol/proj';
 import { Vector as VectorSource } from 'ol/source.js';
 import { Vector as VectorLayer } from 'ol/layer.js';
 import GeoJSON from 'ol/format/GeoJSON.js';
 import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style.js';
 import { defaults as defaultInteractions } from 'ol/interaction.js';
+import {transform} from 'ol/proj';
 
 declare var xepOnline: any;
 declare var jQuery: any;
@@ -68,6 +69,30 @@ export class BasicParcelInfoComponent implements OnInit {
     jQuery('div[id=card]').addClass('Tables card');
   }
 
+  private getBaseMap(type: string) {
+    let source = '';
+    switch (type) {
+      case 'googleLayerRoadNames': source = 'http://mt1.google.com/vt/lyrs=h&x={x}&y={y}&z={z}'; break;
+      case 'googleLayerRoadmap': source = 'http://mt1.google.com/vt/lyrs=h&x={x}&y={y}&z={z}'; break;
+      case 'googleLayerSatellite': source = 'http://mt1.google.com/vt/lyrs=s&hl=pl&&x={x}&y={y}&z={z}'; break;
+      case 'googleLayerHybrid': source = 'http://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}'; break;
+      case 'googleLayerTerrain': source = 'http://mt1.google.com/vt/lyrs=t&x={x}&y={y}&z={z}'; break;
+      case 'googleLayerHybrid2': source = 'http://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}'; break;
+      case 'googleLayerOnlyRoad': source = 'http://mt1.google.com/vt/lyrs=r&x={x}&y={y}&z={z}'; break;
+      case 'OSM': source = 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'; break;
+    }
+    if (source !== '') {
+      return new LayerTile({
+        title: type,
+        source: new XYZ({
+          url: source
+        })
+      });
+    } else {
+      return null;
+    }
+  }
+
   private drawGeometry(geom: any) {
 
     const vs = new VectorSource({
@@ -97,28 +122,24 @@ export class BasicParcelInfoComponent implements OnInit {
       })
     });
 
-    const v = new View();
+    const v = new View({projection: 'EPSG:900913'});
     const polygon = vs.getFeatures()[0].getGeometry();
-    v.fit(polygon);
-
+    v.fit(polygon, { size: [500, 500] });
+    //transform(polygon, 'EPSG:3116', 'EPSG:900913')
     const m = new Map({
       interactions: defaultInteractions({
-        doubleClickZoom: false,
-        dragAndDrop: false,
-        dragPan: false,
-        keyboardPan: false,
-        keyboardZoom: false,
-        mouseWheelZoom: false,
-        pointer: false,
-        select: false
+        doubleClickZoom: true,
+        dragAndDrop: true,
+        dragPan: true,
+        keyboardPan: true,
+        keyboardZoom: true,
+        mouseWheelZoom: true,
+        pointer: true,
+        select: true
       }),
       target: 'map' + this.basicConsult[0].id,
       layers: [
-        new LayerTile({
-          source: new XYZ({
-            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-          })
-        }),
+        this.getBaseMap('googleLayerHybrid'),
         vl
       ],
       view: v

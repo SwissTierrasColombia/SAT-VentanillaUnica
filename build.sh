9592eefb7bf78beb4118be4f8c869a781a01685e
Andres Mauricio Acosta Pulido <andres.taamap@gmail.com>
2019-05-04 14:01:44 -0500

    update

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -ex
 echo "Ejecutando con usuario: $USER y grupo: $GROUPS"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+export PATH=$HOME/local/bin:$PATH
 
 if which nvm >/dev/null; then
   nvm use 10.15.3

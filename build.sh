a79fcbccbaf566414f2cc7906f32b0493eef06c5
Andres Mauricio Acosta Pulido <andres.taamap@gmail.com>
2019-05-04 14:05:16 -0500

    update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,10 @@
 #!/bin/bash -ex
 echo "Ejecutando con usuario: $USER y grupo: $GROUPS"
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-export PATH=$HOME/local/bin:$PATH
 
-if which nvm >/dev/null; then
-  nvm use 10.15.3
+if which n >/dev/null; then
+  n 10.15.3
 else
-  echo 'nvm does not exist'
+  echo 'n does not exist'
 fi
 
 npm install

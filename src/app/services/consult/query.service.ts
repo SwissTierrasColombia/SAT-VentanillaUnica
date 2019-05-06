e9d81e6d4b738de205fd8728d2233a90fd509f6a
Felipe Cano <felipecanol@gmail.com>
2019-05-06 15:40:35 -0500

    Ajuste de las urls de los servicios

--- src/app/services/consult/query.service.ts
+++ src/app/services/consult/query.service.ts
@@ -26,30 +26,30 @@ export class QueryService {
   }
 
   public getBasicConsult(fmi: string, cadastralCode: string, nupre: string) {
-    return this.httpClient.get<BasicParcelInfo>(`${this.apiURL}/query/parcel?fmi=${fmi}&cadastralCode=${cadastralCode}&nupre=${nupre}`, this.httpOptions);
+    return this.httpClient.get<BasicParcelInfo>(`${this.apiURL}/public/parcel?fmi=${fmi}&cadastralCode=${cadastralCode}&nupre=${nupre}`, this.httpOptions);
   }
 
   public getTerrainGeometry(id: number) {
-    return this.httpClient.get<any>(`${this.apiURL}/query/terrain/geometry?id=${id}`, this.httpOptions);
+    return this.httpClient.get<any>(`${this.apiURL}/private/terrain/geometry?id=${id}`, this.httpOptions);
   }
 
   public getParcelGeometry(id: number) {
-    return this.httpClient.get<any>(`${this.apiURL}/query/parcel/geometry?id=${id}`, this.httpOptions);
+    return this.httpClient.get<any>(`${this.apiURL}/private/parcel/geometry?id=${id}`, this.httpOptions);
   }
 
   public getParcelEconomicQuery(fmi: string, cadastralCode: string, nupre: string) {
     const params = `fmi=${fmi}&cadastralCode=${cadastralCode}&nupre=${nupre}`;
-    return this.httpClient.get<EconomicParcelInfo>(`${this.apiURL}/query/parcel/economic?${params}`, this.httpOptions);
+    return this.httpClient.get<EconomicParcelInfo>(`${this.apiURL}/private/parcel/economic?${params}`, this.httpOptions);
   }
 
   public getParcelLegalQuery(fmi: string, cadastralCode: string, nupre: string) {
     const params = `fmi=${fmi}&cadastralCode=${cadastralCode}&nupre=${nupre}`;
-    return this.httpClient.get<LegalParcelInfo>(`${this.apiURL}/query/parcel/legal?${params}`, this.httpOptions);
+    return this.httpClient.get<LegalParcelInfo>(`${this.apiURL}/private/parcel/legal?${params}`, this.httpOptions);
   }
 
   public getParcelPhysicalQuery(fmi: string, cadastralCode: string, nupre: string) {
     const params = `fmi=${fmi}&cadastralCode=${cadastralCode}&nupre=${nupre}`;
-    return this.httpClient.get<PhysicalParcelInfo>(`${this.apiURL}/query/parcel/physical?${params}`, this.httpOptions);
+    return this.httpClient.get<PhysicalParcelInfo>(`${this.apiURL}/private/parcel/physical?${params}`, this.httpOptions);
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   url: 'http://192.168.98.69:8000/rdm',
   KEYCLOAK_URL: 'http://192.168.98.69:8080/auth',
-  geoserver:'http://192.168.98.69:8000/geoserver/'
+  geoserver: 'http://192.168.98.69:8000/geoserver/'
 };

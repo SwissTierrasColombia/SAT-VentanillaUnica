1e15678582e8a0c79f2154f203c5436f847b5882
Felipe Cano <felipecanol@gmail.com>
2019-07-05 12:03:41 -0500

    Ajuste de los enviroments

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  url: 'http://sat.proadmintierra.info/kong/rdm',
-  KEYCLOAK_URL: 'http://sat.proadmintierra.info/auth',
-  geoserver: 'http://sat.proadmintierra.info/geoserver/',
-  imageGeo: 'http://sat.proadmintierra.info/kong/rdm/',
-  qr_base_url: 'http://sat.proadmintierra.info/#/consults/basic-parcel-info',
-};

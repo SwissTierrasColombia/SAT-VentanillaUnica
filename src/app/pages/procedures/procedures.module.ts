a5eaa5f61f0104558be77574287addde89bf5771
Andrés Mauricio Acosta Pulido <andres.taamap@gmail.com>
2019-07-09 15:15:06 -0500

    estructura modal, para agregar restricciones de objetos especiales

--- src/app/pages/procedures/procedures.module.ts
+++ src/app/pages/procedures/procedures.module.ts
@@ -8,14 +8,8 @@ import { FormsModule } from '@angular/forms';
 import { ProcedureRequestComponent } from './procedure-request/procedure-request.component';
 import { ProcedureManagementComponent } from './procedure-managment/procedure-management.component';
 import { RegistroObjEspecialComponent } from '../tramites/registro-obj-especial/registro-obj-especial.component';
-import { DialogBoxComponent } from '../tramites/registro-obj-especial/dialog-box/dialog-box.component';
-import { 
-  MatTableModule, 
-  MatDialogModule, 
-  MatFormFieldModule,
-  MatInputModule,
-  MatButtonModule
-} from '@angular/material';
+import { DialogBoxRegistroComponent } from '../tramites/registro-obj-especial/dialog-box-registro/dialog-box-registro.component';
+
 
 
 @NgModule({
@@ -23,21 +17,12 @@ import {
     ProcedureRequestComponent,
     ProcedureManagementComponent,
     RegistroObjEspecialComponent,
-    DialogBoxComponent
+    DialogBoxRegistroComponent
   ],
   imports: [
     CommonModule,
     ProceduresRoutingModule,
-    FormsModule,
-    MatTableModule,
-    MatDialogModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatInputModule
-  ],
-  entryComponents: [
-    DialogBoxComponent
+    FormsModule
   ]
 
 })
--- src/app/pages/tramites/registro-obj-especial/registro-obj-especial.component.ts
+++ src/app/pages/tramites/registro-obj-especial/registro-obj-especial.component.ts
@@ -1,83 +1,25 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-
-import { MatDialog, MatTable } from '@angular/material';
-import { DialogBoxComponent } from './dialog-box/dialog-box.component';
-
-export interface UsersData {
-  id: number;
-  modelo: string;
-  objeto: string;
-  restricciones: string;
-  urlws: string;
-  fechas: string;
-}
-
-const ELEMENT_DATA: UsersData[] = [
-];
+import { Component, OnInit } from '@angular/core';
+import { ModalService } from 'src/app/services/modal/modal.service';
 
 @Component({
   selector: 'app-registro-obj-especial',
   templateUrl: './registro-obj-especial.component.html',
   styleUrls: ['./registro-obj-especial.component.scss']
 })
+
 export class RegistroObjEspecialComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'modelo', 'objeto', 'restricciones', 'urlws', 'fechas', 'modificar'];
-  dataSource = ELEMENT_DATA;
 
-  @ViewChild(MatTable, {}) table: MatTable<any>;
-
-  constructor(public dialog: MatDialog) { }
-
-  openDialog(modificar: any, obj: { modificar: any; }) {
-    obj.modificar = modificar;
-    const dialogRef = this.dialog.open(DialogBoxComponent, {
-      width: '500px',
-      data: obj
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      if (result.event == 'Add') {
-        this.addRowData(result.data);
-      } else if (result.event == 'Update') {
-        this.updateRowData(result.data);
-      } else if (result.event == 'Delete') {
-        this.deleteRowData(result.data);
-      }
-    });
-  }
-
-  addRowData(row_obj: { modelo: string; objeto: string; restricciones: string; urlws: string; fechas: string }) {
-    var d = new Date();
-    this.dataSource.push({
-      id: d.getTime(),
-      modelo: row_obj.modelo,
-      objeto: row_obj.objeto,
-      restricciones: row_obj.restricciones,
-      urlws: row_obj.urlws,
-      fechas: row_obj.fechas,
-    });
-    this.table.renderRows();
-
-  }
-  updateRowData(row_obj: { id: number; modelo: string; objeto: string; restricciones: string; urlws: string; fechas: string; modificar: string; }) {
-    this.dataSource = this.dataSource.filter((value, key) => {
-      if (value.id == row_obj.id) {
-        value.modelo = row_obj.modelo;
-        value.objeto = row_obj.objeto;
-        value.restricciones = row_obj.restricciones;
-        value.urlws = row_obj.urlws;
-        value.fechas = row_obj.fechas;
-      }
-      return true;
-    });
-  }
-  deleteRowData(row_obj: { id: number; }) {
-    this.dataSource = this.dataSource.filter((value, key) => {
-      return value.id != row_obj.id;
-    });
+  constructor(private modalService: ModalService) {
   }
 
   ngOnInit() {
   }
 
+  openModal(id: string) {
+    this.modalService.open(id);
+  }
+
+  closeModal(id: string) {
+    this.modalService.close(id);
+  }
 }

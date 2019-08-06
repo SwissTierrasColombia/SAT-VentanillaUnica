dd3f5ef79fbde5a7e3d97970d607d53913dd7f84
Andrés Mauricio Acosta Pulido <andres.taamap@gmail.com>
2019-08-06 10:36:44 -0500

    environment configurados

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,10 @@
 
 export const environment = {
   production: false,
-  url: 'http://HOST/kong/rdm', 
-  KEYCLOAK_URL: 'http://HOST/auth',
-  geoserver: 'http://HOST/geoserver/',
-  imageGeo: 'http://HOST/',
+  url: 'http://192.168.98.69:8000/kong/rdm', 
+  KEYCLOAK_URL: 'http://192.168.98.69:8080/auth',
+  geoserver: 'http://192.168.98.69:8000/geoserver/',
+  imageGeo: 'http://192.168.98.69:9090/',
   qr_base_url: 'http://localhost:4200/#/consults/basic-parcel-info',
 };
 

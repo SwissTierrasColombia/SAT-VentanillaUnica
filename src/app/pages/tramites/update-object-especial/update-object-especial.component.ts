aa2d7dffe13380cd69966239c312cc47649cbfb4
Andrés Mauricio Acosta Pulido <andres.taamap@gmail.com>
2019-08-06 18:45:41 -0500

    Se realiza la actualización de un objeto especial

--- src/app/pages/tramites/update-object-especial/update-object-especial.component.ts
+++ src/app/pages/tramites/update-object-especial/update-object-especial.component.ts
@@ -12,19 +12,19 @@ export class UpdateObjectEspecialComponent implements OnInit {
   updateRegister: any
   fechaInicio: Date;
   fechaFinal: Date;
-  formcategories: any;
-  restricciones: any;
+  formcategories = []
+  restricciones = [];
   camposFeature: import("/home/andres/work/SAT-VentanillaUnica/src/app/models/features-object-especial.interface").FeaturesObjectEspecial;
 
   constructor(private services: ObjectEspecialRegimeService) { }
 
   ngOnInit() {
-    console.log(this.updateObject);
+    console.log("OBJ TOTAL:", this.updateObject);
     this.fechaInicio = this.updateObject.objSpecialRegime.createAt
     this.fechaFinal = this.updateObject.objSpecialRegime.dueDate
 
     this.services.GetRestrictions().subscribe(
-      response => {
+      (response: any) => {
         for (let i in response) {
           this.restricciones.push({
             "id": 0,
@@ -32,7 +32,33 @@ export class UpdateObjectEspecialComponent implements OnInit {
             "status": false
           })
         }
-        console.log(this.restricciones);
+        /*         this.restricciones = response
+                for (let i in this.restricciones) {
+                  this.restricciones[i].status = false
+                } */
+        console.log("this.restricciones: ", this.restricciones);
+
+        this.formcategories = this.updateObject.categories
+        for (let i in this.formcategories) {
+          if (this.formcategories[i].restrictions.length < 4) {
+            this.comprobarRestricciones(i)
+          }
+          let auxrestriciones = this.clone(this.restricciones)
+          for (let j in this.formcategories[i].restrictions) {
+            auxrestriciones = auxrestriciones.map(
+              function (item) {
+                if (item.restriction.name == this.formcategories[i].restrictions[j].restriction.name) {
+                  item["status"] = true;
+                }
+                return item;
+              }.bind(this)
+            );
+          }
+          //console.log("auxrestriciones: ", auxrestriciones);
+
+          this.formcategories[i].restrictions = auxrestriciones;
+        }
+
       },
       error => {
         console.error("error restricciones: ", error);
@@ -48,23 +74,25 @@ export class UpdateObjectEspecialComponent implements OnInit {
 
       }
     );
-    this.formcategories = this.updateObject.categories
+  }
 
-    for (let i in this.formcategories) {
-      for (let j = 0; j < this.restricciones.lenght; j++) {
-        if (this.formcategories[i].restrictions[j] == this.restricciones[j]) {
-          this.formcategories[i].restrictions[j].status = true;
-        } else {
-          this.formcategories[i].restrictions[j] = this.restricciones[j].restriction;
-        }
+  comprobarRestricciones(id: string) {
+    for (let index = 0; index < 4; index++) {
+      if (this.formcategories[id].restrictions.length < 4) {
+        this.formcategories[id].restrictions.push({ "restriction": {} })
+      } else {
+        continue
       }
     }
+  }
+  clone(obj: Object) {
+    return JSON.parse(JSON.stringify(obj))
   }
 
   createCategory() {
     this.formcategories.push({
       "category": {
-        "id": this.formcategories.length,
+        "id": 0,
       },
       restrictions: JSON.parse(JSON.stringify(this.restricciones))
     })
@@ -75,10 +103,38 @@ export class UpdateObjectEspecialComponent implements OnInit {
     this.formcategories.splice(id, 1)
   }
 
-  actualizarObjeto() {
-    this.actualizar = false
-  }
   volver() {
     window.location.reload()
   }
+  actualizarObjeto() {
+    let id = this.updateObject.objSpecialRegime.id
+    let idorganization = this.updateObject.objSpecialRegime.organization.id
+    let name = this.updateObject.objSpecialRegime.organization.name;
+    let model = this.updateObject.objSpecialRegime.model;
+    let object = this.updateObject.objSpecialRegime.object;
+    let wsurl = this.updateObject.objSpecialRegime.wsurl;
+    let fechaInicio = this.fechaInicio;
+    let fechaFin = this.fechaFinal;
+    let dataCategorias = JSON.parse(JSON.stringify(this.formcategories))
+    delete dataCategorias.objSpecialRegime
+    for (let i in dataCategorias) {
+      for (let j in dataCategorias[i].restrictions) {
+        delete dataCategorias[i].category.objectSR
+        delete dataCategorias[i].category.restrictions
+        if (dataCategorias[i].restrictions[j].status == false) {
+          delete dataCategorias[i].restrictions[j];
+        } else if (dataCategorias[i].restrictions[j].status == true) {
+          delete dataCategorias[i].restrictions[j].status;
+        }
+        const result = dataCategorias[i].restrictions.filter(word => word != null);
+
+        if (j == '3') {
+          dataCategorias[i].restrictions = result
+        }
+      }
+    }
+    this.services.updateObjectRegister(id, idorganization, name, model, object, wsurl, fechaInicio, fechaFin, dataCategorias);
+    window.location.reload()
+  }
+
 }
